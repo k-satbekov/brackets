86ec30ebd4f79bec30a226c2a0c220faac99b4d7
Kairat Satbekov <kairat.satbekov@Kairats-MacBook-Pro.local>
2020-09-14 13:15:52 +0600

    added full solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = new Array();
+  let bracketMap = {};
+
+  for(const inner of bracketsConfig) {
+    bracketMap[inner[1]] = inner[0];
+  }
+
+  let opening = Object.values(bracketMap);
+  let closing = Object.keys(bracketMap);
+
+  for (let i = 0; i < str.length; i++) {
+    if(closing.includes(str[i]) && opening.includes(str[i])) {
+      if(stack.includes(str[i])) {
+        if(stack[stack.length-1] !== str[i]) return false;
+        else stack.pop();
+      } else {
+        stack.push(str[i]);
+      }
+    } else if(opening.includes(str[i])) { 
+      stack.push(str[i]);
+    } else if(closing.includes(str[i])) {
+        if(stack.length === 0) return false;
+        if(stack.pop() !== bracketMap[str[i]]) {
+          return false;
+        }
+    }
+  }
+    
+  if(stack.length === 0) return true;
+  return false;
 }
